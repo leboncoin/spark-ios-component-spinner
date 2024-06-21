7ede005dd0784b1d2a4eb4b8f94fe98b924b52a5
Robin Lemaire <robin.lemaire@adevinta.com>
2024-06-21 14:23:14 +0200

    Migrate from spark-ios

---
+++ Sources/Testing/SparkSpinnerTesting.swift
@@ -0,0 +1,14 @@
+//
+//  SpinnerViewModel.swift
+//  SparkSpinnerTesting
+//
+//  Created by robin.lemaire on 12/06/2024.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+// IMPORTANT
+// This file is needed because there is
+// no code (without the code generated
+// by sourcery) on this target.
